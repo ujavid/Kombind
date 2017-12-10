61757de38e74a15d8e8828f5629f4d6c39e8b8cf
Robert Keazor <robertkeazor@RobertsMBP87393.fios-router.home>
2017-12-10 10:05:44 -0500

    Remove Second sample

--- app/src/main/java/com/umairjavid/kombindsample/ui/main/SecondActivity.kt
+++
@@ -1,9 +0,0 @@
-package com.umairjavid.kombindsample.ui.main
-
-import com.umairjavid.kombind.ui.KombindActivity
-import com.umairjavid.kombindsample.R
-
-class SecondActivity(override val layoutResId : Int =  R.layout.activity_second) : KombindActivity<SecondViewModel>() {
-    override fun provideViewModelFactory() = SecondViewModel.Factory(application)
-    override val viewModelClass = SecondViewModel::class.java
-}
