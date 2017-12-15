969ede03aaf3d6c709eac7a91ba6cc8919d11551
Umair Javid <ujavid@gmail.com>
2017-12-15 12:04:17 -0500

    Fixed formatting.

--- kombind/src/main/java/com/umairjavid/kombind/model/ViewAction.kt
+++ kombind/src/main/java/com/umairjavid/kombind/model/ViewAction.kt
@@ -6,9 +6,8 @@ import com.umairjavid.kombind.ui.DialogFragmentBuilder
 sealed class ViewAction {
     class StartActivity(val intent: Intent) : ViewAction()
     class StartActivityForResult(val intent: Intent, val requestCode: Int) : ViewAction()
-    class ShowDialog(val builder: DialogFragmentBuilder<*>): ViewAction()
-    class ShowV4Dialog(val builder: com.umairjavid.kombind.ui.v4.DialogFragmentBuilder<*>): ViewAction()
-    class DismissDialog(val tag: String = ""): ViewAction()
-    object Finish: ViewAction()
+    class ShowDialog(val builder: DialogFragmentBuilder<*>) : ViewAction()
+    class ShowV4Dialog(val builder: com.umairjavid.kombind.ui.v4.DialogFragmentBuilder<*>) : ViewAction()
+    class DismissDialog(val tag: String = "") : ViewAction()
+    object Finish : ViewAction()
 }
-
